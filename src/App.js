d4b20ad87b966b1a3dcdeb17e3f2a48a6035a85d
Ange Miklos <angejuba@gmail.com>
2019-08-28 11:41:06 -0700

    adding gregs changes

--- src/App.js
+++ src/App.js
@@ -1,101 +1,120 @@
+    
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+
+class App extends React.Component{
   constructor(){
     super();
-    this.state = {
-     pattern : [],
-     history : [],
-        box1 : false,
-        box2 : false,
-        box3 : false,
-        box4 : false
+    this.state={
+      score:0,
+      clear:false,
+      compPattern:[],
+      userPattern:[],
+      box1:false,
+      box2:false,
+      box3:false,
+      box4:false
     }
   }
 
-
-  handleClick = (id, event) => {
-      let ch = this.state.history;
-      ch.push(id);
-      setTimeout( () => {
-        this.setState({["box" + id] : !(this.state["box" + id]) });
-      }, 300*id );
-      this.setState({ ["box" + id] : !(this.state["box" + id]) });
+  componentDidMount(){
+    this.addRandomNum();
   }
 
-    // the id can be received using event below, OR through the code:
-    // handleClick = (id, event) => or
-    // handleClick(event)   event.target.dataset.id == id
-  handleStart = (id, event) => {
-   // let currentBox = "box" + id;
-   // let currentState = this.state[currentBox];
-   // this.setState({[currentBox] : !currentState});
-    let cp = this.state.pattern;
-    cp.push(Math.floor(Math.random() * 4) + 1);
-    console.log(cp);
-    this.playPattern();
+  componentDidUpdate(){
+    let inputArr = this.state.userPattern;
+    let compArr = this.state.compPattern;
+    let isCorrect=false;
+    let newPattern=[];
+    for(let i=0;i<inputArr.length;i++){
+      if(inputArr[i] === compArr[i]){
+        isCorrect=true;
+      }
+      if(!isCorrect && this.state.clear === false) {
+        this.setState({clear:true})
+        // this.setState({score:"Final Score: " + this.state.score + 1})
+      }
+    }
+    if(compArr.length <= inputArr.length && isCorrect){
+      console.log(true);
+      this.setState({score:this.state.score + 1})
+      let newNum = this.addRandomNum();
+      newPattern = [...inputArr, newNum];
+      this.setState({userPattern:[]});
+      this.showPattern(newPattern);
+    }
   }
-    
-  render() {
-    let wrapperStyle = {display       : "flex",
-                       flexFlow       : "inherit",
-                       justifyContent : "space-around",
-                       flex           : "1 auto",
-                       margin         : 0,
-                       padding        : 0};
-    let startStyle = { background : "center" };
-    return (
-      <div>
+
+  getRandomInt=(min, max)=>{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  addRandomNum=()=>{
+    let randNum = this.getRandomInt(1,5);
+    this.setState({
+      compPattern: [...this.state.compPattern, randNum]
+    })
+    return randNum;
+  }
+
+  showPattern=(currPat)=>{
+    for(let i=0;i<currPat.length;i++){
+      setTimeout(()=>{
+        this.blink(currPat[i]);
+      }, 800*(i+1))
+    }
+  }
+
+  blink=(boxId)=>{
+      this.setState({["box"+boxId]: true});
+      setTimeout(()=>{
+        this.setState({["box"+boxId]: false});
+      }, 300)
+  }
+
+  handleClick=(id)=>{
+    this.blink(id);
+    this.setState({
+      userPattern: [...this.state.userPattern, id]
+    });
+  }
+
+  startFunc=()=>{
+    let compPat = this.state.compPattern;
+    this.showPattern(compPat);
+  }
+
+  render(){
+    console.log("Render", this.state.compPattern, this.state.userPattern)
+    return(
+      <div id="wrapper">
         <h1>Simon</h1>
-        <div className="boxWrapper" style={wrapperStyle}>
-          <Box active={this.state.box1} handleClick={this.handleClick} id={1} color={{num1:255, num2:0, num3:0}}/>
-          <Box active={this.state.box2} handleClick={this.handleClick} id={2} color={{num1:0, num2:255, num3:0}}/>
+        <div>{"Score: " + this.state.score}</div>
+        <div id="boxWrapper" style={{display:this.state.clear ? "none" : "block"}}>
+          <Box active={this.state.box1} handleClick={this.handleClick} id={1} radius={"100px 0 0 0"} color={{num1:255, num2:0, num3:0}}/>
+          <Box active={this.state.box2} handleClick={this.handleClick} id={2} radius={"0 100px 0 0"} color={{num1:0, num2:255, num3:0}}/>
+          <Box active={this.state.box4} handleClick={this.handleClick} id={4} radius={"0 0 0 100px"} color={{num1:255, num2:255, num3:0}}/>
+          <Box active={this.state.box3} handleClick={this.handleClick} id={3} radius={"0 0 100px 0"} color={{num1:0, num2:0, num3:255}}/>
         </div>
-        <div className="boxWrapper" style={wrapperStyle}>
-          <Box active={this.state.box3} handleClick={this.handleClick} id={3} color={{num1:255, num2:255, num3:0}}/>
-          <Box active={this.state.box4} handleClick={this.handleClick} id={4} color={{num1:0, num2:0, num3:255}}/>
-        </div>
-        <button onClick={this.handleStart} style={startStyle}>Start</button>
+        <button onClick={this.startFunc}>Start Game</button>
       </div>
-      );
-  }
-
-  playPattern () {
-    let cp = this.state.pattern;
-    console.log("This is cp: " + cp);
-    for(let i=0; i<cp.length; i++){
-          setTimeout( () => {
-            this.setState({["box" + cp[i]] : !(this.state["box" + cp[i]]) });
-          }, 300*(i+1) );
-        this.setState({["box" + cp[i]] : !(this.state["box" + cp[i]]) });
-    }
+    )
   }
 }
 
-  const Box = (props) => {
-    let opacity = props.active ? "1" : ".5";
-    let bRadius = props.id === 1 ? "30px 0 0 0" : 
-                  props.id === 2 ? "0 30px 0 0" :
-                  props.id === 3 ? "0 0 0 30px" : "0 0 30px 0";
-    let boxStyle = {
-      backgroundColor : "rgba(" + props.color.num1 + "," +
-                                  props.color.num2 + "," + 
-                                  props.color.num3 + ", " + opacity + ")",
-      width        : "50vw",
-      height       : "50vw",
-      borderRadius : bRadius,
-      margin       : "5px",
-      textAlign    : "center"
-    };
-
-    // the id can be passed using html as below, OR through the code:
-    // <div onClick={(event)=>props.handleClick(props.id, event)}
-    // <div data-id={props.id} onClick={props.handleClick} 
-    return (
-      <div onClick={(event)=>props.handleClick(props.id, event)} className="boxStyling" style={boxStyle}>{"box " + props.id}</div>
-      );
+const Box = (props) =>{
+  let opacity = props.active ? ".75" : ".25";
+  let boxStyle = {
+    borderRadius:props.radius,
+    backgroundColor:"rgba("+ props.color.num1 + "," + props.color.num2 + "," + props.color.num3 + "," + opacity + ")"
   }
+  return(
+    <div onClick={()=>{props.handleClick(props.id)}} className="boxStyling" style={boxStyle}></div>
+  )
+}
 
 
 export default App;
